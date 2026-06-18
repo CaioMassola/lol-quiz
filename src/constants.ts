8df9d6a1b242ae8dc3e3899feed42f6e06f575a4
cmassola <cmassola@softexpert.com>
2026-06-18 17:52:53 -0300

    FIX: image timer

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,6 @@ export const CHAMP_DATA_URL = (v: string) => `https://ddragon.leagueoflegends.co
 export const CHAMP_IMG_URL = (v: string, id: string) => `https://ddragon.leagueoflegends.com/cdn/${v}/img/champion/${id}.png`
 export const SPLASH_URL = (id: string) => `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${id}_0.jpg`
 
-export const FLASH_MS = 35
+export const FLASH_MS = 30
 export const ANSWER_MS = 3000
 export const REVEAL_MS = 1200
